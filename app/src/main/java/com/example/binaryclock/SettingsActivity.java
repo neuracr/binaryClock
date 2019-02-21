9ba8f62768823d2d829df9a3b06327a5e168d8f4
Guillaume Weghsteen <neuracr@gmail.com>
2019-02-21 22:35:55 +0100

    using stateListDrawable for the bit on/off drawable. Let us change the bit with the view state

--- app/src/main/java/com/example/binaryclock/SettingsActivity.java
+++ app/src/main/java/com/example/binaryclock/SettingsActivity.java
@@ -1,33 +1,37 @@
 package com.example.binaryclock;
 
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.GridLayoutAnimationController;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
-    private boolean state = true;
+    private int num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Button sw = findViewById(R.id.button);
-        sw.setOnClickListener(new View.OnClickListener() {
+        TextView tv_number = findViewById(R.id.num);
+        tv_number.setText(""+num);
+
+        Button change = findViewById(R.id.change);
+        change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImageView bit = findViewById(R.id.d12);
-                if (state){
-                    bit.setBackgroundDrawable(getDrawable(R.drawable.bit_off));
-                }
-                else{
-                    bit.setBackgroundDrawable(getDrawable(R.drawable.bit_on));
-                }
-                state = !state;
+                View bit = findViewById(R.id.d00);
+                bit.setActivated(!bit.isActivated());
             }
         });
+
 
     }
 }
