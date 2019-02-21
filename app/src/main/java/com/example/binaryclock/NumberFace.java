680a9347463f97e7347d22d0bcbd4f637b82d08b
Guillaume Weghsteen <neuracr@gmail.com>
2019-02-21 23:25:33 +0100

    add NumberFace class to display a number in binary

---
+++ app/src/main/java/com/example/binaryclock/NumberFace.java
@@ -0,0 +1,31 @@
+package com.example.binaryclock;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class NumberFace {
+    private int modulo;
+    private ArrayList<View> bits;
+
+    public NumberFace(ArrayList<View> bits, int modulo){
+        this.modulo = modulo;
+        this.bits = bits;
+
+        for (View v: bits){
+            v.setActivated(false);
+        }
+    }
+
+    public void increment(){
+        for (int i=bits.size()-1; i>=0; i--){
+            if (!bits.get(i).isActivated()){
+                bits.get(i).setActivated(true);
+                break;
+            }
+            else{
+                bits.get(i).setActivated(false);
+            }
+        }
+    }
+}
