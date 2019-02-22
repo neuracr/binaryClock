e8ce60da22f56a610778e274d5733dc9f07f7ad4
Guillaume Weghsteen <neuracr@gmail.com>
2019-02-22 17:47:55 +0100

    add ClockFace class that uses the NumberFace class to display the time

--- app/src/main/java/team23/binaryclock/ClockFace.java
+++ app/src/main/java/team23/binaryclock/ClockFace.java
@@ -2,6 +2,14 @@ package team23.binaryclock;
 
 import team23.binaryclock.NumberFace;
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Defines the main face of the clock, containing some numbers
@@ -11,11 +19,36 @@ public class ClockFace{
     private NumberFace h;
     private NumberFace m;
     private NumberFace s;
+    private TableLayout table;
 
-    public ClockFace(){
-        this.context = context;
-        h = new NumberFace();
-        //m = new NumberFace();
-        //s = new NumberFace();
+    public ClockFace(TableLayout table){
+        this.table = table;
+        Log.i("ClockFace", "number of rows:" + table.getChildCount());
+        Log.i("ClockFace", "number of rows:" + ((TableRow)table.getChildAt(0)).getChildCount());
+
+        ArrayList<View> column = getColumn(0);
+        column.addAll(getColumn(1));
+        this.h = new NumberFace(column, 24);
+        column = getColumn(2);
+        column.addAll(getColumn(3));
+        this.m = new NumberFace(column, 60);
+        column = getColumn(4);
+        column.addAll(getColumn(5));
+        this.s = new NumberFace(column, 60);
+        s.increment();
+        s.increment();
+        s.increment();
+        s.increment();
+        m.increment();
     }
+
+    private ArrayList<View> getColumn(int number){
+        ArrayList<View> column = new ArrayList<View>();
+        for (int i=0 ; i < this.table.getChildCount() ; i++){
+            column.add(((TableRow)this.table.getChildAt(i)).getChildAt(number));
+        }
+        return column;
+    }
+
+
 }
