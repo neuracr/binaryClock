47e2b713eefb44238221fd0e838209b7b9980fe2
Guillaume Weghsteen <neuracr@gmail.com>
2019-02-24 17:15:10 +0100

    add ticking of the clock

--- app/src/main/java/team23/binaryclock/SettingsActivity.java
+++ app/src/main/java/team23/binaryclock/SettingsActivity.java
@@ -1,5 +1,8 @@
 package team23.binaryclock;
 
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +10,12 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class SettingsActivity extends AppCompatActivity {
     private int num = 0;
+    private final static int TICK_MESSAGE = 0;
+    private ClockFace clockFace;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +33,7 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        final ClockFace clockFace = new ClockFace((TableLayout)findViewById(R.id.face));
+        this.clockFace = new ClockFace((TableLayout)findViewById(R.id.face));
 
         Button next = findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
@@ -36,9 +43,44 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        final Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int TICKS_IN_A_ROW = 50;
+                //set the clock for the first time
+                tick.sendEmptyMessage(TICK_MESSAGE);
 
+                //tries to land on the top of each second
+                while(true) {
+                    Calendar c = Calendar.getInstance();
+                    int offset = c.get(Calendar.MILLISECOND);
 
+                    long nextTime = SystemClock.uptimeMillis() + 1000 - offset;
+                    for (int i=0 ; i < TICKS_IN_A_ROW ; i++) {
+                        tick.sendEmptyMessageAtTime(TICK_MESSAGE, nextTime + i*1000);
+                    }
+                    try {
+                        Thread.sleep(TICKS_IN_A_ROW * 1000);
+                    }
+                    catch (InterruptedException e){
+
+                    }
+                }
+            }
+        });
+        t.start();
     }
+
+    final private Handler tick = new Handler(){
+        @Override
+        public void handleMessage(Message m){
+            super.handleMessage(m);
+
+            if (m.what == TICK_MESSAGE) {
+                clockFace.setTime();
+            }
+        }
+    };
 
 
 }
